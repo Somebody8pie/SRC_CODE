485a79af8de4ae3c67e30be9b3bbae814380c0b6
Somebody8pie <Somebody8pie@gmail.com>
2022-06-25 08:20:52 -0500

    dom done

--- WEBDEV/DOM/index.js
+++ WEBDEV/DOM/index.js
@@ -1,6 +1,22 @@
 const para = document.querySelector('.error') // Best way to grab an element from the dom
 
-para.innerHTML = '<h2>This is new</h2>';
+// para.innerHTML = '<h2>This is new</h2>';
+para.setAttribute('class', 'success')
 
-const link = document.querySelector('a')
+let success = para.getAttribute('class')
+
+if(success == 'success')
+{
+    para.setAttribute('style','color: green;')
+    para.innerText = 'success';
+}
+    const link = document.querySelector('a')
 link.setAttribute('href', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+
+const title = document.querySelector('.title')
+
+console.log(title.style)
+title.style.margin = '50px';
+title.style.backgroundColor = '#7c7c7c2b'
+title.style.borderRadius = '10px'
+title.style.textAlign = 'center';
