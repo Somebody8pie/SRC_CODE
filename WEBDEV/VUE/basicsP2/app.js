a99f0f34de16a64ada54c9bd02cd44df83c172f1
Somebody8pie <Somebody8pie@gmail.com>
2022-06-25 16:56:26 -0500

    starting pt2

---
+++ WEBDEV/VUE/basicsP2/app.js
@@ -0,0 +1,34 @@
+const app = Vue.createApp({
+    // data, functions
+    // template: "<h2>I'm a template</h2>"
+    data() {
+        return {
+            title: "James and the Giant Peach",
+            author: "Roald Dahl",
+            publishDate: 1961,
+            clicks: 0,
+            sneakyCount: 0,
+            showSneakyCount: false,
+        }
+    },
+    methods: {
+        publishDateUp() {
+            this.publishDate++
+        },
+        publishDateDown() {
+            this.publishDate--
+        },
+        clicksUp() {
+            this.clicks++
+        },
+        sneakyCountUp() {
+            this.sneakyCount++
+            showSneakyCount = true
+        },
+        toggleShowSneakyCount() {
+            this.showSneakyCount = !this.showSneakyCount
+        },
+    }
+})
+
+app.mount('#app') // # means with the id of app (using id because you can't use that with anything else )
