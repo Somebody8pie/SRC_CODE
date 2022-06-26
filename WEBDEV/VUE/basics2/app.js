0ece8b092ed227af5fba366073bfe2e197f8b056
Somebody8pie <Somebody8pie@gmail.com>
2022-06-26 12:57:53 -0500

    Fireship Node.js

--- WEBDEV/VUE/basics2/app.js
+++ WEBDEV/VUE/basics2/app.js
@@ -3,30 +3,27 @@ const app = Vue.createApp({
     // template: "<h2>I'm a template</h2>"
     data() {
         return {
-            title: "James and the Giant Peach",
-            author: "Roald Dahl",
-            publishDate: 1961,
+            books: [
+                { title: "James and the Giant Peach", author: "Roald Dahl", publishDate: 1961, img: "https://images-na.ssl-images-amazon.com/images/I/81P+Kws4Z-L.jpg" },
+                { title: "When the Sea turned to Sliver", author: "Grace Lin", publishDate: 2016, img: "https://m.media-amazon.com/images/I/61gU-aFg2ZL.jpg" },
+                { title: "Harry Potter and the Philosophers Stone", author: "J.K Rowling", publishDate: 1997, img: "https://images-na.ssl-images-amazon.com/images/I/51mtZy7oJVL.jpg" }
+            ],
             clicks: 0,
-            sneakyCount: 0,
-            showSneakyCount: false,
+            showBooks: false,
+            textShow: "Show books",
+            url: "https://www.youtube.com/channel/UC9rmRAadamujhNqgnMCEMuA/",
         }
     },
     methods: {
-        publishDateUp() {
-            this.publishDate++
-        },
-        publishDateDown() {
-            this.publishDate--
-        },
         clicksUp() {
             this.clicks++
         },
-        sneakyCountUp() {
-            this.sneakyCount++
-            showSneakyCount = true
-        },
-        toggleShowSneakyCount() {
-            this.showSneakyCount = !this.showSneakyCount
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+            this.textShow = "Show books"
+            if(this.showBooks == true) {
+                this.textShow = "Hide books"
+            }
         },
     }
 })
