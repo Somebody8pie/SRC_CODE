b789c26249f595c2714c5b98b819c8f2d850edb8
Somebody8pie <Somebody8pie@gmail.com>
2023-05-27 20:50:12 -0500

    start tilemap

--- PYTHON/PYGAME/main.py
+++ PYTHON/PYGAME/main.py
@@ -1,19 +1,14 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((1000,1000), flags=pygame.SCALED, vsync=1)
+screen = pygame.display.set_mode((1280,720), flags=pygame.SCALED, vsync=1)
 
 clock = pygame.time.Clock()
 running = True
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 enemy_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2 + 100)
-
-maze = [
-    ['0','0','0','0','0'],
-    ['1','1','1','1','1'],
-    ['0','0','0','0','0']
-]
+wall_pic = pygame.image.load('/home/vincent/Documents/WALL.png')
 
 while running:
 
@@ -21,19 +16,7 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    y=0
-    for row in maze:
-        x=0
-        for tile in row:
-            if  tile == 0:
-                screen.blit()
-            x += 1
-        y += 1
-
-    
     player_rect = pygame.Rect(player_pos.x,player_pos.y,60,60)
-    enemy_rect = pygame.Rect(enemy_pos.x,enemy_pos.y,60,60)
-    
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a]:
@@ -49,12 +32,8 @@ while running:
 
 
     screen.fill((250,250,250))
-    pygame.draw.rect(screen, (250,0,0), enemy_rect)
     pygame.draw.rect(screen, (0,0,250), player_rect)
     
-    if player_rect.colliderect(enemy_rect):
-        running = False
-
     pygame.display.flip()
     clock.tick(60)
     
